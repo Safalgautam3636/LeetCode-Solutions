a788cae96d858c5f9cf21c8520b74efd539cdbda
Safal Gautam <63838373+Safalgautam3636@users.noreply.github.com>
2023-04-07 11:40:29 +0545

    Time: 11 ms (46.65%), Space: 55.6 MB (40.54%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,38 +1,34 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        // boolean visited[][]=new boolean[grid.length][grid[0].length];
-        int ans=0;
-        int row=grid.length;
-        int col=grid[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if((i==0||j==0||i==row-1||j==col-1)&&grid[i][j]==1){
-                    dfs(grid,row,col,i,j);
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int ans = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if ((i == 0 || j == 0 || i == row - 1 || j == col - 1) && grid[i][j] == 1) {
+                    dfs(grid, visited, i, j);
                 }
             }
         }
-        return summer(grid);
-    }
-    int summer(int[][]grid){
-        int sum=0;
-        for(int x[]:grid){
-            for(int y:x){
-                sum+=y;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    ans++;
+                }
             }
         }
-        return sum;
+        return ans;
     }
-    void dfs(int[][]grid,int row,int col,int i,int j){
-        if(i>row-1||i<0||j>col-1||j<0||grid[i][j]==0){
+
+    void dfs(int[][] grid, boolean[][] visited, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) {
             return;
         }
-        grid[i][j]=0;
-        // visited[i][j]=true;
-        dfs(grid,row,col,i+1,j);
-        dfs(grid,row,col,i-1,j);
-        dfs(grid,row,col,i,j+1);
-        dfs(grid,row,col,i,j-1);
-        
-        
+        visited[i][j] = true;
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i, j - 1);
     }
 }
