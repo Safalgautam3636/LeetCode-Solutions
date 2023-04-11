9c2489bdd39f065cdd362118af40144b47db0ecb
Safal Gautam <63838373+Safalgautam3636@users.noreply.github.com>
2023-04-11 09:54:34 +0545

    Time: 26 ms (63.12%), Space: 50.7 MB (31.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -9,19 +9,13 @@ class Solution {
     int dfs(String str1,String str2,int n,int m,int[][]dp){
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(i==0||j==0){
-                    dp[i][j]=0;
-                }
+                if(i==0||j==0)dp[i][j]=0;
             }
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
+                if(str1.charAt(i-1)==str2.charAt(j-1))dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);  
             }
         }
         return dp[n][m];
