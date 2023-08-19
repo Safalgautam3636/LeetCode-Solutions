bd1dc092c820526320a8cb11ab4504ecf370e7c3
Safal Gautam <63838373+Safalgautam3636@users.noreply.github.com>
2023-08-19 18:03:11 -0400

    Time: 0 ms (100.00%), Space: 39.4 MB (31.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,23 @@
 class Solution {
-    public int climbStairs(int n) {
-        int one=1;
-        int two=1;
-        for(int i=0;i<n-1;i++){
-            int temp=one;
-            one=one+two;
-            two=temp;
+    public int dpSol(int n,int dp[]){
+        
+        if(n<0){
+            return 0;
         }
-        return one;
+        if(n==0||n==1){
+            return 1;
+        }
+         if(dp[n]!=-1){
+            return dp[n];
+        }
+        else{
+            dp[n]= dpSol(n-1,dp)+dpSol(n-2,dp);
+        }
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return dpSol(n,dp);
     }
 }
