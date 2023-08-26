ecd8991b57a897d8a1b9ea187f67c6ca658425ab
Safal Gautam <63838373+Safalgautam3636@users.noreply.github.com>
2023-08-26 10:11:49 -0400

    Time: 21 ms (70.33%), Space: 44.1 MB (43.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,33 @@
 class Solution {
+   
     public String longestPalindrome(String s) {
-        String longest = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            String palindrome1 = expandAroundCenter(s, i, i);     // Odd-length palindromes
-            String palindrome2 = expandAroundCenter(s, i, i + 1); // Even-length palindromes
-            
-            if (palindrome1.length() > longest.length()) {
-                longest = palindrome1;
-            }
-            
-            if (palindrome2.length() > longest.length()) {
-                longest = palindrome2;
+        int maxLen=0;
+        String str="";
+        for(int i=0;i<s.length();i++){
+            // expand(s,i,i);
+            // expand(s,i,i+1);
+            int start=i;
+            int end=i;
+            while(start>=0 && end<s.length() &&s.charAt(start)==s.charAt(end)){
+                if(end-start+1>maxLen){
+                    maxLen=end-start+1;
+                    str=s.substring(start,end+1);
+                }
+                start--;
+                end++;
+             }
+            start=i;
+            end=i+1;
+            while(start>=0 && end<s.length() &&s.charAt(start)==s.charAt(end)){
+                if(end-start+1>maxLen){
+                    maxLen=end-start+1;
+                    str=s.substring(start,end+1);
+                }
+                start--;
+                end++;
             }
         }
-
-        return longest;
+        return str;
     }
-    
-    private String expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return s.substring(left + 1, right);  // Extract the palindrome substring
-    }
+   
 }
