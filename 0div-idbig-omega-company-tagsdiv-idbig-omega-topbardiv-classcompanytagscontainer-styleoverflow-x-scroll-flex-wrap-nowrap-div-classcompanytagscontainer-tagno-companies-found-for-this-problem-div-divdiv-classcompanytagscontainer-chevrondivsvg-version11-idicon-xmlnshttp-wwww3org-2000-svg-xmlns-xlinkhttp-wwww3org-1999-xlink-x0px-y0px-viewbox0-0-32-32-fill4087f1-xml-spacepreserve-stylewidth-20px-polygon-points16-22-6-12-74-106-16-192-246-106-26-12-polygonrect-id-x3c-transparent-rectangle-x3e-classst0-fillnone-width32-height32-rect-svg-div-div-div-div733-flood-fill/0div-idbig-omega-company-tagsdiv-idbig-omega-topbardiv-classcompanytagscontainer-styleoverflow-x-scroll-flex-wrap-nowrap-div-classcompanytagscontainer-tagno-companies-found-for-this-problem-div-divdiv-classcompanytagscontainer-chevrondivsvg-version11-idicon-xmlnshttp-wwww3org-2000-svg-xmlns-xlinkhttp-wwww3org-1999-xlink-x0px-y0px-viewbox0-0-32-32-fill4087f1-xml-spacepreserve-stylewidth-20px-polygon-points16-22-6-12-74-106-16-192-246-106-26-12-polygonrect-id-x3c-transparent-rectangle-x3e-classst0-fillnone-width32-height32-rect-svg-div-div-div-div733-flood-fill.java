fcb6f9182499dbaee5023170013a135299391410
Safal Gautam <63838373+Safalgautam3636@users.noreply.github.com>
2024-03-09 16:00:42 -0500

    Time: 0 ms (100.00%), Space: 44.6 MB (80.64%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div733-flood-fill.java
@@ -1,30 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int target=image[sr][sc];
-        
         for(int i=0;i<image.length;i++){
-            for(int j=0;j<image[0].length;j++){
-                    if(i==sr&&j==sc){
-                        
-                        dfs(image,target,color,i,j);
-                    }
-                
+            for(int j=0;j<image.length;j++){
+                if(i==sr&&j==sc){
+                    dfs(i,j,image,color,image[i][j]);
+                }
             }
         }
-        
         return image;
     }
-    void dfs(int[][]image,int target,int color,int i,int j){
-       
-        if(i<=image.length-1&&j<=image[0].length-1&&i>=0&&j>=0&&image[i][j]==target&&target!=color){
-                System.out.println(i+""+j);
-                image[i][j]=color;
-               
-                dfs(image,target,color,i+1,j);
-                dfs(image,target,color,i-1,j);
-                dfs(image,target,color,i,j-1);
-                dfs(image,target,color,i,j+1);}
-        
-    }
     
+    void dfs(int i,int j,int[][]image,int color,int from){
+        if(i<=image.length-1&&j<=image[0].length-1&&i>=0&&j>=0&&image[i][j]==from&&color!=from){
+            image[i][j]=color;
+            dfs(i-1,j,image,color,from);
+            dfs(i,j-1,image,color,from);
+            dfs(i+1,j,image,color,from);
+            dfs(i,j+1 ,image,color,from);
+        }
+    }
 }
